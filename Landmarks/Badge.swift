eca25efc8c9cbec899d69be9af5fd01e1ed6c9a9
Carlos E. Barboza <carlos.barboza@fossilizedbits.com>
2020-06-23 22:40:02 -0300

    Drawing and Animation - Drawing Paths and Shapes Cleaned up some files. Section 1. Create a Badge View

---
+++ Landmarks/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Carlos E. Barboza on 6/23/20.
+//  Copyright © 2020 Carlos E. Barboza. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
