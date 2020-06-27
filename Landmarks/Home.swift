5a11487a578730154c6e39b55c0d2006cda486cf
Carlos E. Barboza <carlos.barboza@fossilizedbits.com>
2020-06-27 08:58:12 -0300

    App Design and Layout - Composing Complex Interfaces Section 1. Add a Home View Section 2. Create a Categories List

---
+++ Landmarks/Home.swift
@@ -0,0 +1,34 @@
+//
+//  Home.swift
+//  Landmarks
+//
+//  Created by Carlos E. Barboza on 6/27/20.
+//  Copyright © 2020 Carlos E. Barboza. All rights reserved.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarkData,
+            by: { $0.category.rawValue }
+        )
+    }
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(categories.keys.sorted(), id: \.self) { key in
+                    Text(key)
+                }
+            }
+            .navigationBarTitle(Text("Featured"))
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
